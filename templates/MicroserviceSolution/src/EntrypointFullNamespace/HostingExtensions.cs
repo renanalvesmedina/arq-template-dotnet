b380808451f798e199d34fc8f33068406b29188a
Renan Alves Medina <alvesrenansantos@hotmail.com>
2022-12-26 10:29:14 -0300

    update template

--- templates/MicroserviceSolution/src/EntrypointFullNamespace/HostingExtensions.cs
+++ templates/MicroserviceSolution/src/EntrypointFullNamespace/HostingExtensions.cs
@@ -1,6 +1,4 @@
-#if (IsWebApi)
 using System.Reflection;
-#endif
 
 namespace EntrypointFullNamespace;
 
@@ -8,9 +6,6 @@ public static class HostingExtensions
 {
     public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
     {
-#if (IsWebApi)
-        //This line is only required because there's no reference for Application during configuration
-        //but when you have any reference for application this line is not required
         Assembly.Load("FullNamespace.Application");
 
         builder.Services.AddControllers();
@@ -21,41 +16,7 @@ public static class HostingExtensions
         builder.Services.AddAutoMapper(superAssemblies);
 
         builder.Services.AddSuperMediator(superAssemblies, opt => opt.UseValidation());
-#endif
-#if (IsWorker)
-        builder.Services.AddHostedService<Worker>();
-#endif
-#if (IsHangfire)
-        builder.Services.AddSuperHangfireServer(options => { options.Queues = new[] { "AppNameTemplate" }; });
-#endif
-#if (silverback)
-        //See documentation: https://dev.azure.com/Superdigital-HoldCo/global-platform/_git/arq-lib-common-silverback
-        builder.Services.AddSuperSilverback()
-                .AddAllEndpointsConfigurators<SilverbackEndpointConfigurator>();
-#endif
 
-#if (mongodb)
-        //See documentation: https://dev.azure.com/Superdigital-HoldCo/global-platform/_git/arq-lib-common-mongo
-        builder.Services.AddSuperMongo();
-
-        //Example of adding a collection, you can inject into constructor using IMongoCollection<WeatherForecast>
-        // services.AddMongoCollection<WeatherForecast>(opt => opt
-        //     // If collection does not exist, create with cosmos shard key
-        //     .CreateWithCosmosShardKey(x => x.WeatherForecastId)
-
-        //     // Define indexes
-        //     .HasIndex(b => b.Ascending(x => x.WeatherForecastId), new CreateIndexOptions { Unique = true })
-        //     .HasIndex(b => b.Ascending(x => x.Date))
-
-        //     // Or explicitly say there is no index
-        //     .HasNoIndex()
-        // );
-#endif
-
-#if (redis)
-        //See documentation: https://dev.azure.com/Superdigital-HoldCo/global-platform/_git/arq-lib-common-data?path=/docs/redis.md&_a=preview
-        builder.Services.AddSuperDistributedCache();
-#endif
         return builder;
     }
 }
--- templates/MicroserviceSolution/src/EntrypointFullNamespace/Program.cs
+++ templates/MicroserviceSolution/src/EntrypointFullNamespace/Program.cs
@@ -1,45 +1,28 @@
 using EntrypointFullNamespace;
-#if (IsWebApi)
 using Super.Common.Hosting.WebApi;
-#endif
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.ConfigureSuperHost(options => options.ApplicationName = "AppNameTemplate");
 
-#if (IsWebApi)
 builder.ConfigureSuperWebApi(apiInfo =>
 {
     apiInfo.Title = "TODO: api title";
     apiInfo.Description = "TODO: api description";
 });
-#endif
-#if (IsWorker)
-builder.ConfigureSuperWorker();
-#endif
-#if (IsHangfire)
-builder.ConfigureSuperWorker();
-#endif
+
 builder.ConfigureServices();
 
 var app = builder.Build();
 
-#if (IsWebApi)
 if (OpenApiGenerator.Runner.ShouldExecute(args))
 {
     await OpenApiGenerator.Runner.RunAsync(args, app.Services);
     return;
 }
 
-// Configure the HTTP request pipeline.
-#endif
-
-#if (!IsWebApi)
 app.UseRouting();
-#endif
-#if (IsWebApi)
 app.MapControllers();
-#endif
 
 await app.RunAsync();
